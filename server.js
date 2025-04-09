0dc2fe015e896e52929d5b21998bb2b170051bf5
23DP3SOsip home <A230309SO@rvt.lv>
2025-04-09 20:37:14 +0300

    shmall fixes

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error"));
-db.once("open", () => console.log("✅ Connected to MongoDB"));
+db.on("error", console.error.bind(console, "MongoDB connection problema"));
+db.once("open", () => console.log("piekonektots pie MongoDB"));
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/tasks", taskRoutes);
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Serveris strada uz porta ${PORT}`));
